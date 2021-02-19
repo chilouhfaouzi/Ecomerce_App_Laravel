ff06f2c0fd583de9f5fa7658b6a76d1ccd3d2341
fawzichilouh <fawzichilouh@gmail.com>
2021-02-19 11:31:55 +0100

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+   <h1 class="text-center">Home</h1>
+   <div class="row">
+       <div class="col-6">Col left</div>
+       <div class="col-6">Col right</div>
+   </div>
+</div>
+@endsection
