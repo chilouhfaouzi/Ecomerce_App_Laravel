2d6ff29dd1413131d0764c531a1d6e51fca2a21a
fawzichilouh <fawzichilouh@gmail.com>
2021-02-19 15:00:57 +0100

    upload files

---
+++ storage/framework/views/80992309be30ff375ce5abcfc6f84ecdd88b609f.php
@@ -0,0 +1,11 @@
+<?php $__env->startSection('content'); ?>
+<div class="container">
+   <h1 class="text-center">Home</h1>
+   <div class="row">
+       <div class="col-6">Col left</div>
+       <div class="col-6">Col right</div>
+   </div>
+</div>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\Fawzi\Desktop\laravel_ecomerce_prj\resources\views/home.blade.php ENDPATH**/ ?>
